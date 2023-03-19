bf2f2ce86fa9837b32016007127ea068120e3a1f
suchan <lsuchan0011@naver.com>
2023-03-19 21:58:22 +0900

    blog만들기 초기

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [titles, setTitles] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']); //자바스크립트의 Destruturing 문법
+  const [good, setGood] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+      <div className="list">
+        <h4>
+          {titles[0]}{' '}
+          <span
+            onClick={() => {
+              setGood(good + 1);
+            }}
+          >
+            👍
+          </span>{' '}
+          {good}
+        </h4>
+        <p>3월 19일 발행</p>
+      </div>
+      <div className="list">
+        <h4>
+          {titles[1]} <span>👍</span>
+        </h4>
+        <p>3월 19일 발행</p>
+      </div>
+      <div className="list">
+        <h4>
+          {titles[2]} <span>👍</span>
+        </h4>
+        <p>3월 19일 발행</p>
+      </div>
     </div>
   );
 }
