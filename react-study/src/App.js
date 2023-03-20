e135892ec08f452c82d103fbde04332541637c72
suchan <lsuchan0011@naver.com>
2023-03-21 01:08:06 +0900

    Modal컴포넌트 생성

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -57,13 +57,18 @@ function App() {
         </h4>
         <p>3월 19일 발행</p>
       </div>
-      <div className="modal">
-        <h4>글제목</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div>
+      <Modal />
     </div>
   );
 }
 
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>글제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
+}
 export default App;
