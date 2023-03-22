0ca255ff8dcb0d9f5cfb7adf3f532bbfdbe7db55
suchan <lsuchan0011@naver.com>
2023-03-23 04:10:51 +0900

    데이터 js추가 및 import, 데이터 layout만들기

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Container, Row, Col, Nav, Navbar } from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data.js';
 
 function App() {
+	const [shoes, setShoes] = useState(data);
 	return (
 		<div className='App'>
-			<Navbar bg='dark' variant='dark' className='ps-3'>
+			<Navbar bg='light' variant='light' className='ps-3'>
 				<Navbar.Brand href='#home'>수찬이네</Navbar.Brand>
 				<Nav className='me-auto'>
 					<Nav.Link href='#home'>Home</Nav.Link>
@@ -12,8 +15,26 @@ function App() {
 					<Nav.Link href='#pricing'>Pricing</Nav.Link>
 				</Nav>
 			</Navbar>
+			<div className='main-bg'></div>
+			<Container>
+				<Row>
+					{/* public 폴더에서 이미지 경로 : process.env.PUBLIC_URL + '/logo192.png' */}
+					{shoes.map(function (item, index) {
+						return <Item item={item} index={index} key={index}></Item>;
+					})}
+				</Row>
+			</Container>
 		</div>
 	);
+	function Item(props) {
+		return (
+			<Col>
+				<img src={'https://codingapple1.github.io/shop/shoes' + (props.index + 1) + '.jpg'} alt='' width='80%' />
+				<h4>{props.item.title}</h4>
+				<h6>{props.item.content}</h6>
+				<p>{[props.item.price].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 원</p>
+			</Col>
+		);
+	}
 }
-
 export default App;
---
+++ shop/src/data.js
@@ -0,0 +1,25 @@
+let data = [
+	{
+		id: 0,
+		title: 'White and Black',
+		content: 'Born in France',
+		price: 120000,
+	},
+
+	{
+		id: 1,
+		title: 'Red Knit',
+		content: 'Born in Seoul',
+		price: 110000,
+	},
+
+	{
+		id: 2,
+		title: 'Grey Yordan',
+		content: 'Born in the States',
+		price: 130000,
+	},
+];
+// let data2 = []
+export default data;
+// 여러개 export시에는  =>  export {data, data2}
