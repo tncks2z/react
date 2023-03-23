946fd7d649f4d306203df6d7a6a30a9c226a4da0
suchan <lsuchan0011@naver.com>
2023-03-23 16:24:36 +0900

    router 설치

--- shop/src/App.js
+++ shop/src/App.js
@@ -11,29 +11,30 @@ function App() {
 				<Navbar.Brand href='#home'>수찬이네</Navbar.Brand>
 				<Nav className='me-auto'>
 					<Nav.Link href='#home'>Home</Nav.Link>
-					<Nav.Link href='#features'>Features</Nav.Link>
-					<Nav.Link href='#pricing'>Pricing</Nav.Link>
+					<Nav.Link href='#cart'>Cart</Nav.Link>
 				</Nav>
 			</Navbar>
 			<div className='main-bg'></div>
+			<ItemList shoes={shoes}></ItemList>
+		</div>
+	);
+	function ItemList(props) {
+		return (
 			<Container>
 				<Row>
-					{/* public 폴더에서 이미지 경로 : process.env.PUBLIC_URL + '/logo192.png' */}
-					{shoes.map(function (item, index) {
-						return <Item item={item} index={index} key={index}></Item>;
+					{props.shoes.map(function (item, index) {
+						return (
+							<Col key={index}>
+								{/* public 폴더에서 이미지 경로 : process.env.PUBLIC_URL + '/logo192.png' */}
+								<img src={'https://codingapple1.github.io/shop/shoes' + (index + 1) + '.jpg'} alt='' width='80%' />
+								<h4>{item.title}</h4>
+								<h6>{item.content}</h6>
+								<p>{[item.price].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 원</p>
+							</Col>
+						);
 					})}
 				</Row>
 			</Container>
-		</div>
-	);
-	function Item(props) {
-		return (
-			<Col>
-				<img src={'https://codingapple1.github.io/shop/shoes' + (props.index + 1) + '.jpg'} alt='' width='80%' />
-				<h4>{props.item.title}</h4>
-				<h6>{props.item.content}</h6>
-				<p>{[props.item.price].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 원</p>
-			</Col>
 		);
 	}
 }
