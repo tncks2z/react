cb2fa49ed11753f7df493850501dc4ed5d31ba42
suchan <lsuchan0011@naver.com>
2023-03-24 20:53:51 +0900

    useEffect 실습(숫자만 입력값으로 받도록)

--- shop/src/pages/detail.js
+++ shop/src/pages/detail.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, InputGroup, Form } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 function Detail(props) {
 	const [isAlert, setIsAlert] = useState(true);
-	// useEffect 뒤에 []추가시, mount 될때 1회만 해당코드 재생. 이후엔 노재생
+	const [userInput, setUserInput] = useState('');
+	const [isNumber, setIsNumber] = useState(true);
 	useEffect(
+		// useEffect 뒤에 []추가시, mount 될때 1회만 해당코드 재생. 이후엔 노재생
 		() => {
 			// 두번째로 코드 실행
 			// 재랜더링마다 코드 실행
@@ -23,12 +25,17 @@ function Detail(props) {
 			// 해당 내용 업데이트시 코드 실행
 		]
 	);
-
+	useEffect(() => {
+		const regex = /^[0-9]+$/;
+		setIsNumber(regex.test(userInput));
+	}, [userInput]);
 	const { id } = useParams();
 	const filterItem = props.shoes.find((item) => item.id === Number(id));
+
 	return (
 		<Container>
 			{isAlert ? <div className='alert alert-danger'>2초이내 구매시 할인</div> : null}
+
 			<Row>
 				<Col>
 					<img src={'https://codingapple1.github.io/shop/shoes' + (Number(filterItem.id) + 1) + '.jpg'} alt='' width='100%' />
@@ -40,6 +47,15 @@ function Detail(props) {
 					<button className='btn btn-danger'>주문하기</button>
 				</Col>
 			</Row>
+			<InputGroup className='mb-3'>
+				<Form.Control
+					placeholder='숫자를 입력해주세요'
+					onChange={(e) => {
+						setUserInput(e.target.value);
+					}}
+				/>
+			</InputGroup>
+			{isNumber || userInput === '' ? null : <div className='alert alert-danger'>입력값은 숫자여야만합니다</div>}
 		</Container>
 	);
 }
