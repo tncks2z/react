d6f5ce4b9e46fc422b15168d3cebfc271eea84aa
suchan <lsuchan0011@naver.com>
2023-03-24 19:54:58 +0900

    useEffect 사용법 정리

--- shop/src/pages/detail.js
+++ shop/src/pages/detail.js
@@ -4,11 +4,25 @@ import { useParams } from 'react-router-dom';
 
 function Detail(props) {
 	const [isAlert, setIsAlert] = useState(true);
-	useEffect(() => {
-		setTimeout(() => {
-			setIsAlert(false);
-		}, 2000);
-	});
+	// useEffect 뒤에 []추가시, mount 될때 1회만 해당코드 재생. 이후엔 노재생
+	useEffect(
+		() => {
+			// 두번째로 코드 실행
+			// 재랜더링마다 코드 실행
+			let timer = setTimeout(() => {
+				setIsAlert(false);
+			}, 2000);
+			return () => {
+				// 첫번째로 코드 실 행
+				// 기존코드 없애버릴 때 사용
+				// unMount시 코드 실행
+				clearTimeout(timer);
+			};
+		},
+		[
+			// 해당 내용 업데이트시 코드 실행
+		]
+	);
 
 	const { id } = useParams();
 	const filterItem = props.shoes.find((item) => item.id === Number(id));
