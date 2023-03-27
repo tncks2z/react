6e256f362dc218c5ee27a8faa559aaf40c80708d
suchan <lsuchan0011@naver.com>
2023-03-27 19:45:24 +0900

    axios를 사용한 데이터 get 요청

--- shop/src/App.js
+++ shop/src/App.js
@@ -4,10 +4,11 @@ import { useState } from 'react';
 import data from './data.js';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import Detail from './pages/detail';
-import { Axios } from 'axios';
+import axios from 'axios';
 
 function App() {
-	const [shoes] = useState(data);
+	const [shoes, setShoes] = useState(data);
+	const [moreCnt, setMoreCnt] = useState(0);
 	const navigate = useNavigate();
 	return (
 		<div className='App'>
@@ -54,6 +55,38 @@ function App() {
 					<Route path='two' element={<h4>생일기념 쿠폰 받기</h4>}></Route>
 				</Route>
 			</Routes>
+			{moreCnt === 2 ? null : (
+				<button
+					onClick={() => {
+						moreCnt === 0
+							? axios
+									.get('https://codingapple1.github.io/shop/data2.json')
+									.then((res) => {
+										let copy = [...shoes, ...res.data];
+										setShoes(copy);
+									})
+									.catch((err) => {
+										console.log(err);
+									})
+							: moreCnt === 1
+							? axios
+									.get('https://codingapple1.github.io/shop/data3.json')
+									.then((res) => {
+										let copy = [...shoes, ...res.data];
+										setShoes(copy);
+									})
+									.catch((err) => {
+										console.log(err);
+									})
+							: alert('상품이 더 없습니다');
+						setMoreCnt(moreCnt + 1);
+						// 한번에 두개 이상 axios 요청할때
+						// Promise.all([axios.get('/url1'), axios.get('/url2')])
+						// 	.then((res) => { console.log(res); })
+					}}>
+					더보기
+				</button>
+			)}
 		</div>
 	);
 	function ItemList(props) {
@@ -62,11 +95,11 @@ function App() {
 				<Row>
 					{props.shoes.map(function (item, index) {
 						return (
-							<Col key={index}>
+							<Col md={4} key={index}>
 								{/* public 폴더에서 이미지 경로 : process.env.PUBLIC_URL + '/logo192.png' */}
 								<img src={'https://codingapple1.github.io/shop/shoes' + (index + 1) + '.jpg'} alt='' width='80%' />
 								<h4>{item.title}</h4>
-								<h6>{item.content}</h6>
+								{/* <h6>{item.content}</h6> */}
 								<p>{[item.price].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 원</p>
 							</Col>
 						);
