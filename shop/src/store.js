9b5ead6d080e8f45aed074afeb769c2c3e452649
suchan <lsuchan0011@naver.com>
2023-04-17 18:34:00 +0900

    장바구니 기존 데이터 수정

--- shop/src/store.js
+++ shop/src/store.js
@@ -18,8 +18,8 @@ export let { changeName, changeAge } = user.actions;
 const cart = createSlice({
 	name: 'cart',
 	initialState: [
-		{ id: 0, name: 'White and Black', count: 2 },
-		{ id: 2, name: 'Grey Yordan', count: 1 },
+		// { id: 0, name: 'White and Black', count: 2 },
+		// { id: 2, name: 'Grey Yordan', count: 1 },
 	],
 	reducers: {
 		addCount(state, action) {
